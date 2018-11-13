17282ed0801a6843fa04bf27b28fb10ebbd62d65
Dominic Gagliardi <35349713+DomMacgyver@users.noreply.github.com>
2018-11-13 17:00:02 -0500

    Update and rename lsrl to lsrl.h

--- lsrl.h
+++ lsrl.h
@@ -24,11 +24,15 @@ float[] lsrl(float x[], float y[],int size)
     y0 += y[i];
     y1 += y[i] * x[i];
   }
+
   float b0 = a * y0;
   b0 += b * y1;
   float b1 = b * y0;
   b1 += d * y1;
+  float y_mean = y0;
+  y_mean /= size;
   float error = 0.0;
+  float sst = 0.0
   for(int i = 0;i < size;i++)
   {
     float val = b1 * x[i];
@@ -36,9 +40,17 @@ float[] lsrl(float x[], float y[],int size)
     val -= y[i];
     val *= val;
     error += val;
+    val = y[i];
+    val -= y_mean;
+    val *= val;
+    sst += val;
   }
+  float r = error;
+  r /= sst;
+  r *= -1;
+  r += 1;
   
-  
-
+  float values[] = {b0, b1, r};
+  return values;
 
 }
