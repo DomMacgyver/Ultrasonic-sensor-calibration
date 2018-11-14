7bffa6ac23a1907e60dc03afd90195bf202c2f7e
Dominic Gagliardi <35349713+DomMacgyver@users.noreply.github.com>
2018-11-13 21:08:58 -0500

    Update sketch_nov12_18_ultrasonic_range_sensor.ino
    Better integrated the least squares regression into the calibration

--- sketch_nov12_18_ultrasonic_range_sensor.ino
+++ sketch_nov12_18_ultrasonic_range_sensor.ino
@@ -3,7 +3,7 @@
 int row;
 int x;
 
-const unsigned int size = 5;// number of data points that will be collected
+const unsigned int size = 8;// number of data points that will be collected
 const unsigned int sample_size = 10; //then umber of samples to take for each average; larger numbers for more accuracy, smaller numbers for faster run times
 const unsigned int TRIG_PIN = 7;
 const unsigned int ECHO_PIN = 6;
@@ -14,18 +14,20 @@ bool calibration = true;
 double man_offset = 0.0;// how far away the senosr will be from the wall when measuring since ultrasonic senors cannot get a reading at point blank range, units in cm
 double calibration_offset = 0.0;//in cm
 
-const double interval = 2.0; // the distance between each measurement
+const double interval = 1.5; // the distance between each measurement
 
-//initilaize arrays
+//initialize arrays
+
 double darr[size];// array of pre-measured distances
 double val[size];// array of average distance values from the sensor
-unsigned long durations[size]; //array of average durations, in ms
+double durations[size]; //array of average durations, in ms
 
 //hard coded values for testing
-//double darr[] = {5.0, 7.0, 9.0, 11.0, 13.0};
-//double val[] = {4.2024, 5.6089, 7.9931, 9.9485, 11.8182};
-//unsigned long durations[] = {3311, 11013, 13102, 21010, 22301};
-
+/*
+  double darr[] = {5.0, 7.0, 9.0, 11.0, 13.0};
+  double val[] = {4.2024, 5.6089, 7.9931, 9.9485, 11.8182};
+  double durations[] = {1655.5, 5506.5, 6551, 10505, 11150.5};
+*/
 void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
@@ -49,7 +51,7 @@ double offset_calibrate()
 
     //recive duration from the sensor
     const unsigned long duration = pulseIn(ECHO_PIN, HIGH);
-    distance = duration * 1.0  / 29.15 / 2.0; // calculate the distance in cm: formula: time / speed of sound in cm / 2 because sound travels there and back
+    distance = duration / 2.0 * 0.0342 + 1.101; // calculate the distance in cm: formula: time / 2 because sound travels there and back * the slope of the line + the intercept
     Serial.println(distance, 5);//print the distance
     delay(20);
   }
@@ -101,7 +103,7 @@ void loop() {
 
     unsigned long total = 0;
 
-    Serial.println("Ready");//acnowledeges that input has been recieved
+    Serial.println("Ready");//acknowledges that input has been recieved
     delay(1000);
     Serial.println("Go");
 
@@ -112,10 +114,9 @@ void loop() {
       digitalWrite(TRIG_PIN, HIGH);
       delayMicroseconds(10);
       digitalWrite(TRIG_PIN, LOW);
-      const unsigned long duration = pulseIn(ECHO_PIN, HIGH);
-      //double distance = calibration_offset + duration * 1.0  / 29.15 / 2.0;
+      unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+      duration /= 2;
 
-      // Serial.println(distance, 5);
       if (duration <= 1 || duration >= 200000)
       {
         num -= 1;//don't count this run if the value doesn't make sense
@@ -131,36 +132,43 @@ void loop() {
 
     double average = total / sample_size * 1.0;// compute the average
     durations[position] = average;// add this average durtion to the array
-    double distance = average / 29.15 / 2.0;//calculate the predicted distance
-    val[position] = distance; // stire the predicted distance to the array
+    double distance = average * 0.0342 + 1.101;//calculate the predicted distance
+    val[position] = distance; // store the predicted distance to the array
   }
 
-  //print the data (to 4 decimal places) as three(3) columns (measured on left, sensor in the middle, and durations on right) sperated by commas
+  //print the data (to 5 decimal places) as three(3) columns (measured on left, sensor in the middle, and durations on right) sperated by commas
   Serial.println();
   for (int count = 0; count < size; count++)
   {
-    Serial.print(darr[count], 4);
+    Serial.print(darr[count], 3);
     Serial.print(", ");
-    Serial.print(val[count], 4);
+    Serial.print(val[count], 5);
     Serial.print(", ");
-    Serial.print(durations[count], 4);
+    Serial.print(durations[count], 5);
     Serial.println();
   }
-  // Serial.println(calibration_offset, 5); // print the calibration offset to 5 decimal places
   //helpful site for viewing: http://www.alcula.com/calculators/statistics/scatter-plot/
 
 
   double line_vals[3];
-  lsrl(darr, val, size, line_vals);
+  lsrl(durations, darr, size, line_vals);
   Serial.println();//print a space
 
   //print the least squares regression line coefficents and correlation coefficent (R Squared), to 7 decimal places
-  Serial.print("A0: ");
+  Serial.print("A0(Intercept): ");
   Serial.print(line_vals[0], 7);
-  Serial.print(", A1: ");
+  Serial.print(", A1(slope): ");
   Serial.print(line_vals[1], 7);
   Serial.print(", R Squared: ");
   Serial.print(line_vals[2], 7);
+  Serial.println();
+  //print equation
+  Serial.println("New Equation:");
+  Serial.println();
+  Serial.print("distance = duration / 2.0 * ");
+  Serial.print(line_vals[1], 4);
+  Serial.print(" + ");
+  Serial.print(line_vals[0], 4);
   Serial.println();
   delay(50000);
 }
