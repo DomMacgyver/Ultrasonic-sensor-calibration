18019be4f248c2f374a0ea990ee97f0c40cd99a6
Dominic Gagliardi <35349713+DomMacgyver@users.noreply.github.com>
2018-11-15 18:50:22 -0500

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -1,11 +1,11 @@
 #ifndef Functions_h
 #define Functions_h
 #define swap(a, b) { double t = a; a = b; b = t; }
-void lsrl(double x[], double y[],int dim, double values[])
+void lsrl(double x[], double y[], int dim, double values[])
 {
   double b = 0.0;
   double d = 0.0;
-  for(int i = 0;i < dim;i++)
+  for (int i = 0; i < dim; i++)
   {
     b += x[i];
     d += x[i] * x[i];
@@ -21,7 +21,7 @@ void lsrl(double x[], double y[],int dim, double values[])
 
   double y0 = 0.0;
   double y1 = 0.0;
-  for(int i = 0;i < dim;i++)
+  for (int i = 0; i < dim; i++)
   {
     y0 += y[i];
     y1 += y[i] * x[i];
@@ -36,14 +36,14 @@ void lsrl(double x[], double y[],int dim, double values[])
   y_mean /= dim;
   double error = 0.0;
   double sst = 0.0;
-  for(int i = 0;i < dim;i++)
+  for (int i = 0; i < dim; i++)
   {
     double val = b1 * x[i];
     val += b0;
     val -= y[i];
     val *= val;
     error += val;
-    
+
     val = y[i] - y_mean;
     val *= val;
     sst += val;
@@ -55,4 +55,5 @@ void lsrl(double x[], double y[],int dim, double values[])
   values[2] = r;
 
 }
+
 #endif
